9fa5b10da43b9a46ec270d6966a462dd948dc72b
mtocchetto <marcelo.tocchetto@gmail.com>
2013-10-29 16:00:14 -0200

    Adicionadas informações sobre autor e referência a palestra do evento TDC2013#Porto Alegre.

--- src/main/java/com/company/project/MyStories.java
+++ src/main/java/com/company/project/MyStories.java
@@ -40,6 +40,15 @@ import com.company.project.steps.MySteps;
  * <p>
  * Stories are specified in classpath and correspondingly the {@link LoadFromClasspath} story loader is configured.
  * </p> 
+ * 
+ * <p>##############################</p>
+ * <p>
+ * Classe gerada/adaptada a partir do <a href="http://jbehave.org/reference/stable/archetypes.html">jbehave-simple-archetype</a>.<br/>
+ * Fonte utilizado no evento <a href="http://www.thedevelopersconference.com.br/#portoalegre">TDC2013</a> realizado em Porto Alegre
+ * para a palestra <a href="http://prezi.com/wter-r9fgvqz/?utm_campaign=share&utm_medium=copy&rc=ex0share">"Execução de testes em paralelo com JBehave e Selenium Grid"</a></b>.
+ * </p>
+ * 
+ * @author Marcelo Tocchetto
  */
 public class MyStories extends JUnitStories {
 	
